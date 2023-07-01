cfd44cbace6af32ec7c3dcb65d0e04f4df333dc6
Aninda Nath <nathaninda01@gmail.com>
2023-07-01 14:46:12 +0530

    add client-side file format validation

--- src/UI/fileUpload.js
+++ src/UI/fileUpload.js
@@ -2,15 +2,35 @@ import { useState } from "react";
 
 import styles from './fileUpload.module.css';
 
+const format = ['mp4', 'm4v', 'mov', 'mpg', 'mpeg', 'avi', 'mkv', 'wmv', 'flv', 'webm', 'vob', 'evo', 'mts', 'm2ts'];
+
 const FileUpload = () => {
-    const [ file, setFile] = useState('');
+    const [file, setFile] = useState('');
+    const [error, setError] = useState({
+        state: false,
+        message: ''
+    });
     const [uploading, setUploading] = useState(false);
 
-    const changeHandler = ( event ) => {
-        setFile(event.target.files[0]);
+    const changeHandler = (event) => {
+        const ext = event.target.files[0].name.lastIndexOf('.');
+        if (format.includes(event.target.files[0].name.slice(ext + 1).toLowerCase())) {
+            setError({
+                state: false,
+                message: ''
+            })
+            setFile(event.target.files[0]);
+        }
+        else {
+            setError({
+                state: true,
+                message: 'Unsopported file format'
+            })
+        }
     }
-
+    
     const handleSubmission = (event) => {
+        console.log('done')
         event.preventDefault();
         setUploading(true);
         //axios stuff
@@ -18,10 +38,11 @@ const FileUpload = () => {
     }
 
     return (
-        <form className={styles.card}>
+        <form className={styles.card} onSubmit={(e) => e.preventDefault()}>
+            {error.state && <p className={styles}>{error.message}</p>}
             <label for='upload'>Upload video :</label>
-            <input type="file" name="upload" id='upload' className={styles.btn} onChange={changeHandler}/>
-            <button onClick={handleSubmission} className={styles.btn}>Submit</button>
+            <input type="file" name="upload" id='upload' className={styles.btn} onChange={changeHandler} />
+            <button onClick={handleSubmission} disabled={error.state && file} className={styles.btn}>Upload</button>
         </form>
     )
 }
